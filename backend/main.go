27345290c6d1233a9cda5755500ec3c7e5d4a93b
j <jforsberg.se@gmail.com>
2026-02-01 20:56:21 +0100

    Change to backend, added log message

--- backend/main.go
+++ backend/main.go
@@ -212,6 +212,8 @@ func (app *App) createEntryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	
+	console.log("This is a message for testing purpose, executes when you post.")
+	
 	// Invalidera cache
 	if app.Redis != nil {
 		app.Redis.Del(app.Ctx, "entries:all")
